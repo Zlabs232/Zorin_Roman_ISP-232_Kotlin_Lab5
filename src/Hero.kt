7624b0fff41675ba3564514d82c5617712740738
Роман Зорин <sgena170315@gmail.com>
2025-09-14 11:00:25 +0300

    Были добавлены примеры работы с объектом класса Hero

--- src/Hero.kt
+++ src/Hero.kt
@@ -1,2 +1,7 @@
 class Hero {
+    var name: String = "Неизвестный"
+    var gender: String = "Не указан"
+    var role: String = "Бродяга"
+    var hp: Int = 100
+    var mp: Int = 50
 }
--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,26 @@
 fun main(){
+    val chmo: Hero = Hero()
+    println("Имя: ${chmo.name} \nПол: ${chmo.gender} \nКласс: ${chmo.role}\nЗдоровье: ${chmo.hp}\nМана: ${chmo.mp}")
 
+    val chuhan = Hero()
+    chuhan.name = "Чухан "
+    chuhan.gender = "Мужской"
+    chuhan.role = "Уборщик"
+    chuhan.hp = 120
+    chuhan.mp = 180
+    println("Имя: ${chuhan.name} \nПол: ${chuhan.gender} \nКласс: ${chuhan.role} \nЗдоровье: ${chuhan.hp} \nМана: ${chuhan.mp}")
+
+    val hero = Hero()
+    print("Введите имя героя: ")
+    hero.name = readln()
+    print("Введите пол героя: ")
+    hero.gender = readln()
+    print("Введите класс героя: ")
+    hero.role = readln()
+    print("Введите здоровье героя: ")
+    hero.hp = readln().toInt()
+    print("Введите ману героя: ")
+    hero.mp = readln().toInt()
+
+    println("Имя: ${hero.name} \nПол: ${hero.gender} \nКласс: ${hero.role} \nЗдоровье: ${hero.hp} \nМана: ${hero.mp}")
 }
